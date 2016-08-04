076f4c573b9763da37d81b05080abcd89b9c2c33
kim.jinhoon <dnvy0084@gmail.com>
2016-08-04 23:34:29 +0900

    주석추가

--- babel/dist/bundle.js
+++ babel/dist/bundle.js
@@ -105,11 +105,21 @@
 	new Main().run();
 
 	// babel transpile : es2015 코드를 CommonJS 코드로 바꿔준다. 
-	// babel js/ -f dist/ 로 컴파일 가능. 
+	// babel js/ -d dist/ 로 컴파일 가능. 
+	// node_modules 
+	// babel-cli, babel-preset-2015
 
 	// browserify bundle : 각각의 file로 나눠져 있는 js 파일들을 
 	// CommonJS module import 규칙에 맞게 하나의 파일로 합친다. 
 	// browserify js/main.js -t babelify -o dist/bundle.js
+	// node_modules
+	// babelify
+
+	// webpack bundle: 각각의 file로 나눠져 있는 js 파일들을 
+	// CommonJS module import 규칙에 맞게 하나의 파일로 합친다. 
+	// webpack.config.js 참고 터미널에서 : webpack 입력
+	// node_modules
+	// babel-loader, babel-core
 
 /***/ },
 /* 1 */
